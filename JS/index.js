41f2db7b40beef4cb3f87cbacc91e10d269428a0
l0st-idi0t <carlnod121@gmail.com>
2023-04-16 11:50:06 -0500

    transition

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,16 @@
 //hehe you don't get to select any text
 document.body.style.userSelect = 'none';
+
+function nextPage() {
+    const name = document.getElementById('name-input').value;
+
+    localStorage.setItem('username', name);
+
+    const transitionCover = document.createElement('div');
+    transitionCover.className = 'transition-cover';
+    document.body.appendChild(transitionCover);
+
+    setTimeout(()=> {
+        window.location.href = 'HTML/dashboard.html';
+    }, 1000);
+}
